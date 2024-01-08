28a959e60d8a91563d8b74bbfe554fcdace3da6b
AnjaliKriti <kritianjali@gmail.com>
2024-01-08 15:46:06 +0530

    Adding code

---
+++ src/main/java/org/example/service/NumberOfCities.java
@@ -0,0 +1,58 @@
+package org.example.service;
+
+
+//    city              count           region
+//        Delhi             4M               North
+//        Mumbai        7M               West
+//        Chennai    6M               South
+//        Chandigarh  2M               North
+//        Kolkata            17M                  East
+//        for a given region or multiple region - list the number of  cities grouped by population range
+//        Population Ranges are  1M to 5M   and 6M to 10M   and  above 10M
+//        Example ->
+//        Input ->
+//        All Region
+//        Output -
+//        1M-to-5M     6M-to-10M  above 10M
+//        2                       2
+
+import org.example.module.Population;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NumberOfCities {
+    public static void main(String[] args) {
+
+        Population population ;
+        List<Population>  populationList = new ArrayList<>();
+        populationList.add(new Population("Delhi", 8L,"North"));
+        populationList.add(new Population("Mumbai", 7L,"West"));
+        populationList.add(new Population("Chennai", 6L,"South"));
+        populationList.add(new Population("Chandigarh", 2L,"North"));
+        populationList.add(new Population("Kolkata", 17L,"East"));
+
+        Map populationWithRange =  findNumberOfCitiesInRegion(populationList , "North" );
+        System.out.println(populationWithRange);
+
+    }
+
+    private static Map findNumberOfCitiesInRegion(List<Population> populationList, String region ) {
+
+        Map<String , Long> populationWithRange  =  populationList.stream().filter(population -> population.getRegion() == region ).
+                collect(Collectors.groupingBy(cities -> {
+                    if (cities.getCount() >= 1L && cities.getCount() <= 5L)
+                        return "1M-5M";
+                    else if (cities.getCount() > 5L && cities.getCount() <= 10L) {
+                        return "5M-10M";
+                    } else if (cities.getCount() > 10L ) {
+                        return "above 10M";
+                    }
+                    else return "Invalid";
+                },Collectors.counting()));
+        return populationWithRange;
+    }
+
+}
---
+++ src/main/java/org/example/service/RemoveDuplicate.java
@@ -0,0 +1,21 @@
+package org.example.service;
+
+import java.util.Arrays;
+
+public class RemoveDuplicate {
+
+
+// remove duplicate from Array, without using collections and without creating new array
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[index]) {
+                arr[index + 1] = arr[i];
+                index++;
+            }
+        }
+        Arrays.stream(arr, 0, index + 1).forEach(i -> System.out.println(i));
+    }
+
+}
