ea16490a388dfd09b6b0c982f38cfaffe7f89e78
AnjaliKriti <kritianjali@gmail.com>
2024-02-12 20:30:49 +0530

    Adding explanation

--- src/main/java/org/example/service/Numbers/PowerOfTwo.java
+++ src/main/java/org/example/service/Numbers/PowerOfTwo.java
@@ -23,3 +23,33 @@ public class PowerOfTwo {
         return (n & (n - 1)) != 0;
     }
 }
+//
+//
+//Explanation :
+//Input : 128
+//n = 128 and n-1 = 127
+//convert into bit
+//128:
+//        Step 1: 128 / 2 = 64, remainder = 0
+//        Step 2: 64 / 2 = 32, remainder = 0
+//        Step 3: 32 / 2 = 16, remainder = 0
+//        Step 4: 16 / 2 = 8, remainder = 0
+//        Step 5: 8 / 2 = 4, remainder = 0
+//        Step 6: 4 / 2 = 2, remainder = 0
+//        Step 7: 2 / 2 = 1, remainder = 0
+//        Step 8: 1 / 2 = 0, remainder = 1
+//  128 in decimal = 10000000 in binary
+//
+//127:
+//        Step 1: 127 / 2 = 63, remainder = 1
+//        Step 2: 63 / 2 = 31, remainder = 1
+//        Step 3: 31 / 2 = 15, remainder = 1
+//        Step 4: 15 / 2 = 7, remainder = 1
+//        Step 5: 7 / 2 = 3, remainder = 1
+//        Step 6: 3 / 2 = 1, remainder = 1
+//        Step 7: 1 / 2 = 0, remainder = 1
+// 127 in decimal = 1111111 in binary
+//
+//now perform AND operation on bit 10000000 & 1111111 = 0
+
+
